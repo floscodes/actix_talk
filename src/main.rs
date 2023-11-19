3b5ef2e600c45203e657792dfd38e1aabd840796
floscodes <petautschnig.florian@gmail.com>
2023-11-19 16:47:38 +0100

    changed pres

--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,3 @@ async fn main() -> Result<(), Error> {
     // .bind(("0.0.0.0", 8000)).unwrap()
     .run().await
 }
-
-
-HttpServer
-
-    App
-        URI-Path  -> Handler Function
-        "/"       -> fn()
-        "/hello"  -> fn()
-
-SET IP AND PORT
-
-RUN APP
